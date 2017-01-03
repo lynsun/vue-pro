6fd754f60b49a87283dfb3769c71c5ad89df0bc3
知春 <jianpeng.sjp@alibaba-inc.com>
2017-01-03 16:18:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <hello></hello>
+    <v-button>button</v-button>
+  </div>
+</template>
+
+<script>
+import atui from 'atui';
+import 'atui/dist/greater-blue.css';
+
+import Hello from './components/Hello';
+
+const vButton = atui.Button;
+
+export default {
+  name: 'app',
+  components: {
+    Hello,
+    vButton,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+
+}
+</style>
