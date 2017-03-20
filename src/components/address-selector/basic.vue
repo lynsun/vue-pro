ad9f86912a3265ce740b4a190d4feca018e05b44
lynsun <814253326@qq.com>
2017-03-20 12:43:23 +0800

    chore: save

---
+++ src/components/address-selector/basic.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+<script>
+  import Vue from 'vue'
+  import atui from 'atui'
+  import 'atui/dist/greater-blue.css'
+
+  console.log(atui)
+  // 全局注册atui组件
+  Vue.use(atui)
+
+  export default{
+    components: {
+      
+    },
+    data() {
+      return {
+        
+      }
+    },
+    methods: {
+      
+    }
+  }
+</script>
+
+<style type="text/css">
+  body {
+    padding: 20px;
+  }
+</style>
